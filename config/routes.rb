9362a6af1cfd52ad9d0f96baf8762dc9823574a9
yuto-yoshimuta <yuuto819midorimatu@gmail.com>
2024-08-01 12:42:20 +0000

    [Finish]4-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  get '/top' => 'homes#top'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+
+  get 'top' => 'homes/top'
+  resources :lists
 end
